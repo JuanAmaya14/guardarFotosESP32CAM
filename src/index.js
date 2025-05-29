c31b22bc718c3e6b564fe848134c7be54a61c69a
Amayap14 <juan.amaya.p14@gmail.com>
2025-05-28 23:59:38 -0500

    index.js puede generar la carpeta donde recibe las imagenes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 require('dotenv').config({ path: __dirname + '/../.env' });
 const express = require("express");
 const rutas = require("./routes/Rutas");
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
 app.set("appName", "ImagenesESP32CAM");
 app.set("host", process.env.APP_HOST);
 app.set("port", process.env.APP_PORT);
+
+// Crear carpeta uploads si no existe
+const uploadsDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
 
 async function startServer() {
   try {
